a04e30b52585d32f6233c722c56ce33c75d022a4
shivarkr <97501320+shivarkr@users.noreply.github.com>
2024-06-27 12:46:03 +0530

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '0c3ad52703aef22b1667a4080d9303cb',
+    apiKey: 'write your api key here',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
